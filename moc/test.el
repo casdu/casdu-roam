68d616314614a99aaccfb7deff39161c61fc781a
Your Name <you@example.com>
2022-07-29 15:35:38 -0400

    update

--- moc/test.el
+++ moc/test.el
@@ -1,2 +1,5 @@
-(find-file "./test.org")
+(add-to-list 'package-archives '("gnu-devel" . "https://elpa.gnu.org/devel/") t)
+(package-initialize)
+(find-file "./index.org")
+(org-transclusion-add-all)
 (org-latex-export-to-pdf)
